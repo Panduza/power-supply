acd60d5aa7a7655d5c5d9940c6820aa4658320f0
XdoctorwhoZ <xavier.rodriguezalvarez@gmail.com>
2025-09-28 22:24:51 +0200

    up

--- src/runner.rs
+++ src/runner.rs
@@ -1,4 +1,17 @@
+use std::time::Duration;
+
+use rand::{distributions::Alphanumeric, Rng};
+use rumqttc::{AsyncClient, MqttOptions};
+
 use crate::drivers::PowerSupplyDriver;
+
+fn generate_random_string(length: usize) -> String {
+    let rng = rand::thread_rng();
+    rng.sample_iter(Alphanumeric)
+        .take(length)
+        .map(|c| c as char)
+        .collect()
+}
 
 struct Runner {
     name: String,
@@ -11,6 +24,15 @@ impl Runner {
     }
 
     pub fn start(self) {
+        let mut mqttoptions = MqttOptions::new(
+            format!("rumqtt-sync-{}", generate_random_string(5)),
+            "localhost",
+            1883,
+        );
+        mqttoptions.set_keep_alive(Duration::from_secs(3));
+
+        let (client, event_loop) = AsyncClient::new(mqttoptions, 100);
+
         // Start the runner
 
         // self.driver.enable_output()
