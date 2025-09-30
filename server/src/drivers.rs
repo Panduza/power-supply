299e9ae6bb8c19eef57dd9185c32658db2f5a838
Xavier RODRIGUEZ <xavier.rodriguezalvarez@gmail.com>
2025-09-30 18:14:01 +0200

    up

--- server/src/drivers.rs
+++ server/src/drivers.rs
@@ -15,4 +15,16 @@ pub trait PowerSupplyDriver: Send + Sync {
     async fn output_enabled(&mut self) -> Result<bool, DriverError>;
     async fn enable_output(&mut self) -> Result<(), DriverError>;
     async fn disable_output(&mut self) -> Result<(), DriverError>;
+
+    //
+    async fn get_voltage(&mut self) -> Result<String, DriverError>;
+    async fn set_voltage(&mut self, voltage: String) -> Result<(), DriverError>;
+
+    //
+    async fn get_current(&mut self) -> Result<String, DriverError>;
+    async fn set_current(&mut self, current: String) -> Result<(), DriverError>;
+
+    //
+    async fn measure_voltage(&mut self) -> Result<String, DriverError>;
+    async fn measure_current(&mut self) -> Result<String, DriverError>;
 }
--- server/src/drivers/emulator.rs
+++ server/src/drivers/emulator.rs
@@ -1,29 +1,59 @@
+use crate::drivers::DriverError;
 use crate::{config::PowerSupplyConfig, drivers::PowerSupplyDriver};
 use async_trait::async_trait;
 
 pub struct PowerSupplyEmulator {
     state_oe: bool,
+    voltage: String,
+    current: String,
 }
 
 impl PowerSupplyEmulator {
     pub fn new(config: PowerSupplyConfig) -> Self {
-        Self { state_oe: false }
+        Self {
+            state_oe: false,
+            voltage: "0".into(),
+            current: "0".into(),
+        }
     }
 }
 
 #[async_trait]
 impl PowerSupplyDriver for PowerSupplyEmulator {
-    async fn output_enabled(&mut self) -> Result<bool, crate::drivers::DriverError> {
+    async fn output_enabled(&mut self) -> Result<bool, DriverError> {
         Ok(self.state_oe)
     }
 
-    async fn enable_output(&mut self) -> Result<(), crate::drivers::DriverError> {
+    async fn enable_output(&mut self) -> Result<(), DriverError> {
         self.state_oe = true;
         Ok(())
     }
 
-    async fn disable_output(&mut self) -> Result<(), crate::drivers::DriverError> {
+    async fn disable_output(&mut self) -> Result<(), DriverError> {
         self.state_oe = false;
         Ok(())
+    }
+
+    async fn get_voltage(&mut self) -> Result<String, DriverError> {
+        Ok(self.voltage.clone())
+    }
+    async fn set_voltage(&mut self, voltage: String) -> Result<(), DriverError> {
+        self.voltage = voltage;
+        Ok(())
+    }
+
+    async fn get_current(&mut self) -> Result<String, DriverError> {
+        Ok(self.current.clone())
+    }
+    async fn set_current(&mut self, current: String) -> Result<(), DriverError> {
+        self.current = current;
+        Ok(())
+    }
+
+    async fn measure_voltage(&mut self) -> Result<String, DriverError> {
+        Ok("0".into())
+    }
+    async fn measure_current(&mut self) -> Result<String, DriverError> {
+        Ok("0".into())
     }
 }
