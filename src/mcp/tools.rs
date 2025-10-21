e1783e047834c87fc6e6b327c1a58480d78288b4
Xavier RODRIGUEZ <xavier.rodriguezalvarez@gmail.com>
2025-10-21 13:36:44 +0200

    up

--- src/mcp/tools.rs
+++ src/mcp/tools.rs
@@ -19,8 +19,8 @@ use serde::{Deserialize, Serialize};
 use tracing::debug;
 use tracing::info;
 
-use panduza_power_supply_client::PowerSupplyClient;
-use panduza_power_supply_client::PowerSupplyClientBuilder;
+use crate::client::PowerSupplyClient;
+use crate::client::PowerSupplyClientBuilder;
 
 use crate::config::GlobalConfig;
 
